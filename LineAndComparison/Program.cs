66181048cd363baac434256f8945cdbffc65af7c
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-01 19:31:06 +0530

    Added : Calculate Length

--- LineAndComparison/Program.cs
+++ LineAndComparison/Program.cs
@@ -6,7 +6,11 @@ namespace LineAndComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Line And Comparison");
+            int x1 = 4;
+            int y1 = 3;
+            int x2 = 6;
+            int y2 = 5;
+            Console.WriteLine(Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
         }
     }
 }
