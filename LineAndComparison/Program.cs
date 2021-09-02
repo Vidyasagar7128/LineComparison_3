59780cc4571758f130c6c9ae67d50e0ee3534068
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-02 08:47:39 +0530

    Added : Check Equality

--- LineAndComparison/Program.cs
+++ LineAndComparison/Program.cs
@@ -10,7 +10,18 @@ namespace LineAndComparison
             int y1 = 3;
             int x2 = 6;
             int y2 = 5;
-            Console.WriteLine(Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+            double length1;
+            double length2;
+            int m1 = 4;
+            int n1 = 3;
+            int m2 = 6;
+            int n2 = 5;
+            length1 = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            length2 = Math.Sqrt((m2 - m1) * (m2 - m1) + (n2 - n1) * (n2 - n1));
+            Console.WriteLine(length1);
+            Console.WriteLine(length2);
+            Console.WriteLine(length1.ToString().Equals(length2.ToString()));
+
         }
     }
 }
